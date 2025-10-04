2b0ad5c1e1372127b643021b4a6e8359bf39e438
alisonmyers <alisonlouisemyers@gmail.com>
2025-10-04 12:10:27 -0400

    remove schema helpers - bloat

--- src/cd_transform_project_data.py
+++ src/cd_transform_project_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sys
-from .helpers import create_folder, schema_rename_and_drop_columns
+from .helpers import create_folder
 from . import settings
 from . import cd_data_details
 
@@ -9,7 +9,6 @@ from . import cd_data_details
 def transform_data(detail_dict, drop_rest=False):
     # TODO ADD DATE TO FILE AS HEADER
     file = f'{detail_dict["name"]}'
-    schema_file = f'{settings.SCHEMAS_FOLDER}/{file}.txt'
     rename_dict = detail_dict["rename_dict"]
 
     in_file =  f'{settings.ORIGINALDATA_FOLDER}/{file}.csv'
@@ -23,10 +22,6 @@ def transform_data(detail_dict, drop_rest=False):
     
     print(f'\nREADING: {in_file}\n')
     df = pd.read_csv(in_file)
-
-    print('## Column Changes')
-    df, data_dict = (df.
-         pipe(schema_rename_and_drop_columns, rename_dict=rename_dict, schema_file=schema_file, drop_rest=drop_rest))
 
     print(f'\nWRITING: {out_file}.csv\n')
     df.to_csv(f'{out_file}', index=False)
@@ -46,8 +41,6 @@ def transform_project_data_fn():
     transform_data(cd_data_details.ASSIGNMENTS_DICT, True)
     transform_data(cd_data_details.MODULEITEMS_DICT, True)
     transform_data(cd_data_details.MODULES_DICT, True)
-#    transform_data(data_details.PAGES_DICT, True)
-#    transform_data(data_details.QUIZZES_DICT, True)
     transform_data(cd_data_details.ASSIGNMENTSUBMISSIONS_DICT, True)
     transform_data(cd_data_details.ENROLLMENTS_DICT, True)
     transform_data(cd_data_details.NEWANALYTICS_NEW_DICT, True)
