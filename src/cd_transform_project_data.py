a9542b04ced6c110319522f782f6b63ef56676bf
alisonmyers <alisonlouisemyers@gmail.com>
2025-10-04 12:38:32 -0400

    safely remove schema tracking and transformations

--- src/cd_transform_project_data.py
+++ src/cd_transform_project_data.py
@@ -3,6 +3,7 @@ import sys
 from .helpers import create_folder
 from . import settings
 from . import cd_data_details
+from .helpers import clean_columns_from_rename_dict
 
 
 
@@ -14,21 +15,13 @@ def transform_data(detail_dict, drop_rest=False):
     in_file =  f'{settings.ORIGINALDATA_FOLDER}/{file}.csv'
     out_file = f'{settings.CLEANEDDATA_FOLDER}/{file}.csv'
 
-
-    # tem = sys.stdout
-    # sys.stdout = f = open(f'{settings.CLEANDDATA_TRACKING_TRANSFORMATIONS}/{file}.md', 'w')
-    
-    # print(f'# {file}')
-    
-    print(f'\nREADING: {in_file}\n')
     df = pd.read_csv(in_file)
 
-    print(f'\nWRITING: {out_file}.csv\n')
+    df = (df.
+         pipe(clean_columns_from_rename_dict, rename_dict=rename_dict, drop_rest=drop_rest))
+
+    #print(f'\nWRITING: {out_file}.csv\n')
     df.to_csv(f'{out_file}', index=False)
-    #data_dict[data_dict['change_note']!='deleted'].to_csv(f'{settings.CLEANDDATA_TRACKING_TRANSFORMATIONS}/{file}_schema.csv', index=False)
-    
-    # sys.stdout = tem
-    # f.close()
     
     return(df)
 
@@ -36,13 +29,13 @@ def transform_data(detail_dict, drop_rest=False):
 
 def transform_project_data_fn():
     create_folder(settings.CLEANEDDATA_FOLDER)
-    create_folder(settings.CLEANDDATA_TRACKING_TRANSFORMATIONS)
+    #create_folder(settings.CLEANDDATA_TRACKING_TRANSFORMATIONS)
 
     transform_data(cd_data_details.ASSIGNMENTS_DICT, True)
     transform_data(cd_data_details.MODULEITEMS_DICT, True)
     transform_data(cd_data_details.MODULES_DICT, True)
     transform_data(cd_data_details.ASSIGNMENTSUBMISSIONS_DICT, True)
-    transform_data(cd_data_details.ENROLLMENTS_DICT, True)
+    transform_data(cd_data_details.ENROLLMENTS_DICT, True) 
     transform_data(cd_data_details.NEWANALYTICS_NEW_DICT, True)
     transform_data(cd_data_details.GRADEBOOKUSERDATA_DICT, True)
 
--- src/helpers.py
+++ src/helpers.py
@@ -725,18 +725,40 @@ def create_df_and_csv(paginatedlist, data_dict, output_folder, iteration_call=No
         print(f'{e}')
         return(data_dict.update({"df": None, "raw_csv": None}))
 
-        
-def transform_to_dict(string):
-    """For reading and writing to dict a schema .txt file, copied and pasted from Canvas Live API"""
-    pattern = "(.*) \((.*), (.*)\): (.*)"
-    prog = re.compile(pattern)
-    result = prog.match(string)
-
-    my_dict = {"name": result.group(1),
-               "data_type": result.group(2),
-               "field_description": result.group(4)} 
-           
-    return(my_dict)
 
 def get_pretty_print(json_object):
     return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
+
+def clean_columns_from_rename_dict(df, rename_dict, drop_rest=False): 
+    # get column information
+
+    og_cols = df.columns.to_list()
+    keep_cols = list(rename_dict.keys())
+    unlisted_cols = list(set(og_cols) - set(keep_cols))
+    has_dropped = False
+    
+    # changes if dropping 
+    if drop_rest:
+        # print("DROPPING UNLISTED COLS")
+        dropped_cols = unlisted_cols
+        # change record
+        if dropped_cols:
+            has_dropped = True
+            dropped_df = pd.DataFrame(dropped_cols)
+            dropped_df.columns = ['original']
+            dropped_df['change_note'] = 'deleted'
+
+        # changed_cols_df = changed_cols_df.append(dropped_df, ignore_index=True)
+        # make change
+        df = df[keep_cols].copy() 
+    
+    else:
+        # print("KEEPING UNLISTED COLS")
+        unlisted_dict = dict(zip(unlisted_cols, unlisted_cols))
+        rename_dict.update(unlisted_dict)
+    
+    #print(changed_cols_df.to_markdown())
+    df.rename(rename_dict, axis=1, inplace=True)
+    #return(df, changed_cols_df)
+    return(df)
+    
